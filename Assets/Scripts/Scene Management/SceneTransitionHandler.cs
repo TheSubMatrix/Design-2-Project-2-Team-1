0fe4804e40e0739f25eac7d64e3e9bdfc2f79098
Colin Whiteford <colinwhiteford516@gmail.com>
2025-10-02 16:12:28 -0400

    Created Animated Scene Transitioner

---
+++ Assets/Scripts/Scene Management/SceneTransitionHandler.cs
@@ -0,0 +1,45 @@
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneTransitionHandler : PersistentSingleton<SceneTransitionHandler>
+{
+    static readonly int Curvature = Shader.PropertyToID("_Curvature");
+    public bool IsTransitioning { get; private set; } 
+    [SerializeField] Material m_crtMaterial;
+
+    public void TransitionScene(string sceneName, float transitionTime = 0.5f)
+    {
+        if(IsTransitioning) return;
+        StartCoroutine(TransitionSceneWithEffectAsync(sceneName, transitionTime));
+    }
+
+    public void ReloadScene(float transitionTime = 0.5f)
+    {
+        TransitionScene(SceneManager.GetActiveScene().name, transitionTime);
+    }
+
+    IEnumerator TransitionSceneWithEffectAsync(string sceneName, float transitionTime = 0.5f)
+    {
+        IsTransitioning = true;
+        float startingCurvature = m_crtMaterial.GetFloat(Curvature);
+        yield return AnimateCRTAsync(transitionTime, 0.0f);
+        yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+        yield return AnimateCRTAsync(transitionTime, startingCurvature);
+        IsTransitioning = false;
+    }
+
+    IEnumerator AnimateCRTAsync(float transitionTime, float destinationCurvature)
+    {
+        float elapsedTime = 0.0f;
+        float startingCurvature = m_crtMaterial.GetFloat(Curvature);
+        while (elapsedTime < transitionTime)
+        {
+            elapsedTime += Time.deltaTime;
+            m_crtMaterial.SetFloat(Curvature, Mathf.SmoothStep(startingCurvature, destinationCurvature, elapsedTime / transitionTime));
+            yield return null;
+        }
+    }
+}
