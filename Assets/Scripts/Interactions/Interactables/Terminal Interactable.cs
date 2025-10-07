f504382d91c4992ddf19f0f237510d6c1331bf95
Colin Whiteford <colinwhiteford516@gmail.com>
2025-10-07 00:17:42 -0400

    Fixed Terminal Interaction Bug

--- Assets/Scripts/Interactions/Interactables/Terminal Interactable.cs
+++ Assets/Scripts/Interactions/Interactables/Terminal Interactable.cs
@@ -1,30 +1,66 @@
 using System;
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.UI;
+using UnityEngine.UI;
 
 public class TerminalInteractable : MonoBehaviour, IInteractable
 {
     CinemachineCamera m_previousCamera;
+    [SerializeField] Selectable m_startingSelectable;
     [SerializeField] CinemachineCamera m_interactionCamera;
-    [SerializeField] InputSystemUIInputModule m_inputModule;
     [SerializeField] InputActionReference m_exitAction;
-
+    [SerializeField] InputActionReference m_moveAction;
+    [SerializeField] InputActionReference m_selectAction;
+    bool m_isInteracting;
     void OnEnable()
     {
         m_exitAction.action.Enable();
         m_exitAction.action.performed += ExitInteractionViaInput;
+        m_moveAction.action.Enable();
+        m_moveAction.action.performed += OnMoveAction;
+        m_selectAction.action.Enable();
+        m_selectAction.action.performed += OnSelectAction;
     }
     void OnDisable()
     {
         m_exitAction.action.Disable();
         m_exitAction.action.performed -= ExitInteractionViaInput;
+        m_moveAction.action.Disable();
+        m_moveAction.action.performed -= OnMoveAction;
+        m_selectAction.action.Disable();
+        m_selectAction.action.performed -= OnSelectAction;
     }
 
     void ExitInteractionViaInput(InputAction.CallbackContext context)
     {
+        if(!m_isInteracting) return;
         OnExitedInteraction();
+    }
+
+    void OnMoveAction(InputAction.CallbackContext context)
+    {
+        if(!m_isInteracting) return;
+        Vector2 input = context.ReadValue<Vector2>();
+        
+        Selectable currentSelectable = EventSystem.current.currentSelectedGameObject?.GetComponent<Selectable>();
+        if (currentSelectable == null)
+            currentSelectable = m_startingSelectable;
+        
+        Vector3 direction = new Vector3(input.x, input.y, 0f);
+        Selectable nextSelectable = currentSelectable.FindSelectable(direction);
+
+        if (nextSelectable != null)
+        {
+            EventSystem.current.SetSelectedGameObject(nextSelectable.gameObject);
+        }
+    }
+
+    void OnSelectAction(InputAction.CallbackContext context)
+    {
+        if(!m_isInteracting) return;
+        ExecuteEvents.Execute(EventSystem.current.currentSelectedGameObject, new BaseEventData(EventSystem.current), ExecuteEvents.submitHandler);
     }
     
     public void OnStartedInteraction(MonoBehaviour interactor)
@@ -34,14 +70,16 @@ public class TerminalInteractable : MonoBehaviour, IInteractable
         EventBus<CameraBlendData>.Raise(new CameraBlendData(m_interactionCamera));
         EventBus<UpdatePlayerInputState>.Raise(new UpdatePlayerInputState(false));
         EventBus<FadeCanvasGroup>.Raise(new FadeCanvasGroup("Hands", 0.5f,0));
-        m_inputModule.enabled = true;
+        EventSystem.current.SetSelectedGameObject(m_startingSelectable.gameObject);
+        m_isInteracting = true;
     }
+
     public void OnExitedInteraction()
     {
         EventBus<CameraBlendData>.Raise(new CameraBlendData(m_previousCamera));
         EventBus<UpdatePlayerInputState>.Raise(new UpdatePlayerInputState(true));
-        EventBus<FadeCanvasGroup>.Raise(new FadeCanvasGroup("Hands", 0.5f,1));
-        m_inputModule.enabled = false;
+        EventBus<FadeCanvasGroup>.Raise(new FadeCanvasGroup("Hands", 0.5f, 1));
+        EventSystem.current.SetSelectedGameObject(null);
+        m_isInteracting = false;
     }
-    
 }
