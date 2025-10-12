921d87e7821b7524ecba8f2d1be87d548d3d4322
Colin Whiteford <colinwhiteford516@gmail.com>
2025-10-12 14:02:45 -0400

    Sanity Pickups

--- Assets/Scripts/Pickups/Sanity Pickup.cs
+++ Assets/Scripts/Pickups/Sanity Pickup.cs
@@ -2,11 +2,9 @@ using UnityEngine;
 
 public class SanityPickup : MonoBehaviour, IInteractable
 {
-    [SerializeField] uint m_healAmount = 10;
+    [SerializeField] int m_sanityChange = 10;
     public void OnStartedInteraction(MonoBehaviour interactor)
     {
-        Sanity healable = interactor.transform.root.GetComponentInChildren<Sanity>();
-        healable?.Heal(m_healAmount);
-        Destroy(gameObject);
+        EventBus<RequestSanityChange>.Raise(new RequestSanityChange(m_sanityChange));
     }
 }
