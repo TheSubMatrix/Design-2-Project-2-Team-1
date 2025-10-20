68d8106b6ea3aed942748104fdbcab06416bc357
Colin Whiteford <colinwhiteford516@gmail.com>
2025-10-20 18:44:32 -0400

    The Numbers Mason, What Do They Mean?

--- Assets/Scripts/Combat Actions/Raycast Shot Fire Action.cs
+++ Assets/Scripts/Combat Actions/Raycast Shot Fire Action.cs
@@ -11,6 +11,7 @@ public class RaycastShotFireAction : BaseCombatAction
     [SerializeField] SoundData m_shotSound;
     [SerializeField] Vector2 m_spread = new Vector2(0f, 0f);
     [SerializeField] UnityEvent m_onShotFired = new();
+    [SerializeField] uint m_damage = 10;
     public override void InitializeCombatAction()
     {
         
@@ -22,7 +23,7 @@ public class RaycastShotFireAction : BaseCombatAction
         if (!Physics.Raycast(m_raycastOrigin.position, Quaternion.AngleAxis(Random.Range(-m_spread.x, m_spread.x), m_raycastOrigin.up) * Quaternion.AngleAxis(Random.Range(-m_spread.y, m_spread.y), m_raycastOrigin.right) * m_raycastOrigin.forward, out RaycastHit hit, m_raycastDistance, m_raycastLayerMask)) yield break;
         m_onShotFired?.Invoke();
         if (!hit.collider.TryGetComponent(out IDamageable damageable)) yield break;
-        damageable.Damage(10);
+        damageable.Damage(m_damage);
         yield return null;
     }
 }
